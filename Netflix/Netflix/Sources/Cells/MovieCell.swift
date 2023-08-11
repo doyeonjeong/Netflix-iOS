598a0d0e84fcb42bde9fc0f99620ac81a2277542
doyeonjeong <debby_@kakao.com>
2023-08-12 01:28:54 +0900

    ➕[Add]: MovieCell

---
+++ Netflix/Netflix/Sources/Cells/MovieCell.swift
@@ -0,0 +1,53 @@
+//
+//  MovieCell.swift
+//  Netflix
+//
+//  Created by DOYEON JEONG on 2023/08/09.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .gray
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK: - Setup
+extension MovieCell {
+    
+    private func setup() {
+        addSubviews()
+        configureViews()
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(imageView)
+    }
+    
+    private func configureViews() {
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+}
