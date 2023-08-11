03d0c20cacc2d64328f8fd93acbe0e4d64b149fd
doyeonjeong <debby_@kakao.com>
2023-08-12 00:58:53 +0900

    ➕[Add]: Home 탭 제외한 기본 ViewController

---
+++ Netflix/Netflix/Sources/Controllers/MoreViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MoreViewController.swift
+//  Netflix
+//
+//  Created by DOYEON JEONG on 2023/08/10.
+//
+
+import UIKit
+
+class MoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
