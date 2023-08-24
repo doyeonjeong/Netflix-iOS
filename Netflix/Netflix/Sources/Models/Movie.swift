3d7ea3abcd1e4f6b41648d28257e7829d83b12cf
doyeonjeong <debby_@kakao.com>
2023-08-24 14:38:25 +0900

    🐛[Fix]: Movie 관련 구조체 수정

--- Netflix/Netflix/Sources/Models/Movie.swift
+++ Netflix/Netflix/Sources/Models/Movie.swift
@@ -7,108 +7,47 @@
 
 import Foundation
 
-struct MovieItem: Hashable {
-    var index: Int // 0...9
-    var title: String
-    var posterURL: String
-    var section: Section
-    
-    init(index: Int, title: String, posterURL: String, section: Section) {
-        self.index = index
-        self.title = title
-        self.posterURL = posterURL
-        self.section = section
-    }
+struct MovieResponse: Codable {
+    let page: Int
+    let results: [Movie]
 }
 
-struct Movie: Hashable {
-    
-    var identifier = UUID()
-    var headerItem: MovieItem
-    var dailyList: [MovieItem]
-    var newKoreaMovieList: [MovieItem]
-    var newUSAMovieListResult: [MovieItem]
-    
-    static func == (lhs: Movie, rhs: Movie) -> Bool {
-        return lhs.identifier == rhs.identifier
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(identifier)
-        hasher.combine(headerItem)
-        hasher.combine(dailyList)
-        hasher.combine(newKoreaMovieList)
-        hasher.combine(newUSAMovieListResult)
-    }
-}
-
-struct MovieInformations: Codable {
-    var boxOfficeResult: BoxOfficeResult?
-    var newKoreaMovieResult: KmdbForNewKoreaMovieResult?
-    var newUSAMovieResult: NewUSAMovieResult?
-}
-
-// MARK: - BoxOfficeResult
-struct BoxOfficeResult: Codable {
-    let dailyBoxOfficeList: [DailyBoxOfficeList]
-}
-
-// MARK: - DailyBoxOfficeList
-struct DailyBoxOfficeList: Codable {
-    let rank: String
-    let movieNm: String
+struct Movie: Codable, Hashable {
+    let uuid = UUID()
+    let id: Int
+    let title: String?
+    let posterPath: String?
+    var isMainItem: Bool = false
     
     enum CodingKeys: String, CodingKey {
-        case rank
-        case movieNm
-    }
-}
-
-// MARK: - NewKoreaMovieResult
-struct NewKoreaMovieResult: Codable {
-    let movieList: [MovieList]
-}
-
-// MARK: - BoxOfficeResult
-struct NewUSAMovieResult: Codable {
-    let movieList: [MovieList]
-}
-
-struct MovieList: Codable {
-    let movieNm: String
-
-    enum CodingKeys: String, CodingKey {
-        case movieNm
-    }
-}
-
-// MARK: - KmdbForNewKoreaMovieResult
-struct KmdbForNewKoreaMovieResult: Codable {
-    let totalCount: Int
-    let data: [Datum]
-
-    enum CodingKeys: String, CodingKey {
-        case totalCount = "TotalCount"
-        case data = "Data"
-    }
-}
-
-// MARK: - Datum
-struct Datum: Codable {
-    let result: [Result]
-
-    enum CodingKeys: String, CodingKey {
-        case result = "Result"
-    }
-}
-
-// MARK: - Result
-struct Result: Codable {
-    let title: String
-    let posters: String
-
-    enum CodingKeys: String, CodingKey {
+        case id
         case title
-        case posters
+        case posterPath = "poster_path"
     }
+}
+
+struct MovieDetailResponse: Codable {
+    let id: Int
+    let title: String?
+    let overview: String?
+    let posterPath: String?
+    let releaseDate: String?
+    let runtime: Int
+    let genres: [Genre]
+    let status: String?
+    let voteAverage: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id, runtime
+        case title, overview, status
+        case genres
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+    }
+}
+
+struct Genre: Codable {
+    let id: Int
+    let name: String?
 }
